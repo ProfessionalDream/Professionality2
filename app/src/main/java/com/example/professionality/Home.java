a3bb8bc7106f26196bf19208bc87410229773aff
MARIO <uhmec@newy.com>
2020-08-07 10:07:35 +0200

    Ho risolto dei problemi+Ho aggiunto EasyGoto per collegare le schermate facilmente M
    Signed-off-by: MARIO <uhmec@newy.com>

--- app/src/main/java/com/example/professionality/Home.java
+++ app/src/main/java/com/example/professionality/Home.java
@@ -1,8 +1,8 @@
 package com.example.professionality;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +10,6 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        new EasyGoto(this,R.id.button3,categorie.class);
     }
 }
