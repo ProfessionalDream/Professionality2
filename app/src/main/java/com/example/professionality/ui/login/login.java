f93ebf5312e7abd8554670c5925b157e084793e0
MARIO <uhmec@newy.com>
2020-08-10 08:14:00 +0200

    Ho risolto dei problemi+Ho aggiunto EasyGoto per collegare le schermate facilmente M
    Signed-off-by: MARIO <uhmec@newy.com>

--- app/src/main/java/com/example/professionality/ui/login/login.java
+++ app/src/main/java/com/example/professionality/ui/login/login.java
@@ -1,17 +1,8 @@
 package com.example.professionality.ui.login;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
@@ -23,16 +14,23 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.professionality.register;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.professionality.Home;
 import com.example.professionality.MainActivity;
 import com.example.professionality.R;
-import com.example.professionality.ui.login.LoginViewModel;
-import com.example.professionality.ui.login.LoginViewModelFactory;
-import com.example.professionality.Home;
+import com.example.professionality.register;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
+    private FirebaseAuth mAuth;
 
     public void enterGuest(View v){
         boolean ok=guest();
@@ -60,6 +58,7 @@ public class login extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth=FirebaseAuth.getInstance();
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
@@ -155,5 +154,10 @@ public class login extends AppCompatActivity {
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 }
